f7e2b57d8ff057fa15d97a05c63a46baa8cf4c21
mehmet sait ışık <saitisik@Sait-MacBook-Pro.local>
2024-11-13 02:28:44 +0300

    ui fix

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,6 @@
 // components/MonthlyRevenueChart.js
 import React from "react";
-import { Line } from "react-chartjs-2";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   LineElement,
@@ -8,15 +8,18 @@ import {
   LinearScale,
   PointElement,
   Tooltip,
+  BarElement,
+  ArcElement,
   Legend,
 } from "chart.js";
-import SidebarLayout from "@/layouts/sidebar-layout";
 
 ChartJS.register(
   LineElement,
   CategoryScale,
   LinearScale,
   PointElement,
+  ArcElement,
+  BarElement,
   Tooltip,
   Legend
 );
@@ -64,7 +67,11 @@ const Dashboard = () => {
   };
 
   return (
+    <div className=" flex flex-col m-12 gap-12">
+      <Bar data={data} options={options} />
       <Line data={data} options={options} />
+      <Doughnut data={data} options={options} />;
+    </div>
   );
 };
 
--- src/pages/users.js
+++ src/pages/users.js
@@ -1,4 +1,3 @@
-// pages/admin.js
 import { useState } from "react";
 import Header from "@/components/header";
 import Modal from "@/components/commons/modal";
@@ -6,12 +5,6 @@ import BaseTable from "@/components/base-table";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import SidebarLayout from "@/layouts/sidebar-layout";
-
-const schema = yup.object().shape({
-  name: yup.string().required("Kullanıcı adı zorunludur"),
-  gender: yup.string().required("Cinsiyet zorunludur"),
-});
 
 const Users = () => {
   const [users, setUsers] = useState([
@@ -19,6 +12,11 @@ const Users = () => {
     { id: 2, name: "Köpek Cesur Işık", gender: "Erkek" },
   ]);
   const [modal, setModal] = useState(false);
+
+  const schema = yup.object().shape({
+    name: yup.string().required("Kullanıcı adı zorunludur"),
+    gender: yup.string().required("Cinsiyet zorunludur"),
+  });
 
   const {
     register,
@@ -43,73 +41,67 @@ const Users = () => {
   };
 
   return (
-      <div className="flex h-screen bg-white text-black">
-        <div className="flex flex-col w-full p-10 gap-2">
-          <Header title="Users Management" />
-          <BaseTable
-            head={[{ title: "Kişi Adı" }, { title: "Kişi Cinsiyeti" }]}
-            isLoading={false}
-            body={users?.map((item) => [item?.name, item?.gender])}
-            searchable={true}
-            automaticPagination={true}
-            tableBtn={
-              <div className="border border-black text-center p-1 rounded-lg w-20 my-2">
-                <button onClick={() => setModal(true)}>Ekle</button>
-              </div>
-            }
-          />
+    <div className="flex h-screen bg-white text-black">
+      <div className="flex flex-col w-full p-10 gap-2">
+        <Header title="Users Management" />
+        <BaseTable
+          head={[{ title: "Kişi Adı" }, { title: "Kişi Cinsiyeti" }]}
+          isLoading={false}
+          body={users?.map((item) => [item?.name, item?.gender])}
+          searchable={true}
+          automaticPagination={true}
+          tableBtn={
+            <div className="border border-black text-center p-1 rounded-lg w-20 my-2">
+              <button onClick={() => setModal(true)}>Ekle</button>
+            </div>
+          }
+        />
 
-          {modal && (
-            <Modal
-              title="Yeni Kullanıcı Ekle"
-              hideModal={() => setModal(false)}
-            >
-              <form
-                onSubmit={handleSubmit(handleAddUser)}
-                className="space-y-4"
-              >
-                <div>
-                  <label className="block text-sm font-medium">
-                    Kullanıcı Adı
-                  </label>
-                  <input
-                    type="text"
-                    {...register("name")}
-                    className="border rounded w-full p-2"
-                  />
-                  {errors.name && (
-                    <p className="text-red-500 text-sm mt-1">
-                      {errors.name.message}
-                    </p>
-                  )}
-                </div>
-                <div>
-                  <label className="block text-sm font-medium">Cinsiyet</label>
-                  <select
-                    {...register("gender")}
-                    className="border rounded w-full p-2"
-                  >
-                    <option value="">Seçiniz</option>
-                    <option value="Erkek">Erkek</option>
-                    <option value="Kadın">Kadın</option>
-                  </select>
-                  {errors.gender && (
-                    <p className="text-red-500 text-sm mt-1">
-                      {errors.gender.message}
-                    </p>
-                  )}
-                </div>
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white p-2 rounded w-full mt-4"
+        {modal && (
+          <Modal title="Yeni Kullanıcı Ekle" hideModal={() => setModal(false)}>
+            <form onSubmit={handleSubmit(handleAddUser)} className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium">
+                  Kullanıcı Adı
+                </label>
+                <input
+                  type="text"
+                  {...register("name")}
+                  className="border rounded w-full p-2"
+                />
+                {errors.name && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.name.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <label className="block text-sm font-medium">Cinsiyet</label>
+                <select
+                  {...register("gender")}
+                  className="border rounded w-full p-2"
                 >
-                  Ekle
-                </button>
-              </form>
-            </Modal>
-          )}
-        </div>
+                  <option value="">Seçiniz</option>
+                  <option value="Erkek">Erkek</option>
+                  <option value="Kadın">Kadın</option>
+                </select>
+                {errors.gender && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.gender.message}
+                  </p>
+                )}
+              </div>
+              <button
+                type="submit"
+                className="bg-black text-white hover:bg-gray-800 p-2 rounded w-full mt-4"
+              >
+                Ekle
+              </button>
+            </form>
+          </Modal>
+        )}
       </div>
+    </div>
   );
 };
 
